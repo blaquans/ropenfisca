9f8a4db5e8ac1bd7ecb8500d766cf1387786714a
blaquans <pachevalier@gmail.com>
2014-03-22 10:24:39 +0100

    upate

--- R/fields.R
+++ R/fields.R
@@ -3,4 +3,25 @@ fields  <- fromJSON(readLines("http://api.openfisca.fr/api/1/fields"))
 str(fields)
 names(fields)
 fields$apiVersion
-View(fields$columns)
+fields$url
+fields$method
+
+columns  <- fields$columns
+names(colums)
+columns$entity$birth
+columns$entity$choi
+columns$"@type"$birth
+columns$label$birth
+columns$label$sali
+columns$cerfa_field$sali
+columns$min$sali
+columns$max$sali
+
+prestations  <- fields$prestations
+names(prestations)
+prestations$default$alleg_cice
+prestations$entity$alleg_cice
+prestations$label$alleg_cice
+prestations$name$alleg_cice
+prestations$labels$0
+ 
--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 ropenfisca
 ==========
 
-A set of functions to use [OpenFisca](openfisca.fr) with [R](http://cran.r-project.org/)
+Just a set of files to use [OpenFisca](openfisca.fr) with [R](http://cran.r-project.org/)
+
+## Files
+* R/main.R load all the packages you need
+* R/fields.R read the [documentation of the API](http://api.openfisca.fr/api/1/fields) and returns to data frames, one for all the fields you can choose as input and one for all the fields you can choose as output.
 
 ## Documentation
 * [All fields in the API](http://api.openfisca.fr/api/1/fields)
